d34dff1afd2e2b6e50b14c8bbf05c02f829398d8
unknown <sagarsamanta390@gmail.com>
2021-09-23 21:40:05 +0530

    add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast=(latitude,longitude,callback)=>{
        }else if(response.body.error){
             callback('Data not found !',undefined)
        }else{
-        const result=response.body.current.weather_descriptions+' .Its currently '+ response.body.current.temperature +' degree out and its feels like '+ response.body.current.feelslike +' degree out';
+           
+        const result=response.body.current.weather_descriptions+' .Its currently '+ response.body.current.temperature +' degree(F) out and its feels like '+ response.body.current.feelslike +' degree(F) out here and humidity is '+response.body.current.humidity;
         callback(undefined,result);
     }
     })
